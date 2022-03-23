86e39276cb1794f0f2ae33cb80151581799f8b20
FloatingComet62 <shubh2007rai@gmail.com>
2022-03-23 23:44:29 +0530

    Setup.py Added

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name="netxpy",
+    version='0.0.1',
+    author="StudiousGamer, FloatingComet62",
+    author_email="<natyavidhanbiswas10@gmail.com>",
+    description='',
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'web', 'websites'],
+    classifiers=[
+        "Development Status :: 1 - Prototype",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
